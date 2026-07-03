de6c1eb74f8237091e61407289aa2ca9e9c0bda4
CarlosV-work <carlos.vep.work@gmail.com>
2026-07-03 11:49:58 -0400

    completado

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include <DHT.h>
+#define DHTPIN 15
+#define DHTTYPE DHT22
+DHT dht(DHTPIN,DHTTYPE);
+
+const int pinLDR = 34;
+const int ledRojo = 27;
+const int ledAmarillo = 14;
+const int ledVerde = 12;
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+  pinMode(ledRojo, OUTPUT);
+  pinMode(ledAmarillo, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+}
+
+void loop()
+{
+  int valorLDR = analogRead(pinLDR);
+  Serial.print("Valor LDR: ");
+  Serial.println(valorLDR);
+  if(valorLDR > 2500){
+    digitalWrite(ledRojo, HIGH);
+    Serial.println("Estado: Oscuridad detectada. LED ENCENDIDO");
+  }else{
+    digitalWrite(ledRojo, LOW);
+    Serial.println("Estado: Hay luz ambiental. LED APAGADO");
+  }
+
+  float temperatura = dht.readTemperature();
+  
+  if(isnan(temperatura)){
+    Serial.println("Error al leer el sensor DHT22");
+    return;
+  }
+  Serial.print("Temperatura actual: ");
+  Serial.println(temperatura);
+  if(temperatura < 28.0){
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(ledAmarillo, LOW);
+    if(valorLDR < 2500){
+      digitalWrite(ledRojo, LOW);
+    }
+    Serial.println("Estado: clima agradable");
+  }else if(temperatura >= 28.0 && temperatura <= 34.0){
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarillo, HIGH);
+    if(valorLDR < 2500){
+      digitalWrite(ledRojo, LOW);
+    }
+    Serial.println("Estado: clima moderado");
+  }else{
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarillo, LOW);
+    digitalWrite(ledRojo, HIGH);
+    Serial.println("Estado: clima caluroso");
+  }
+  delay(2000);
+}

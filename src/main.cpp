cfb770cdc729613530c042ffba7862e1e370c880
CarlosV-work <carlos.vep.work@gmail.com>
2026-07-03 20:04:52 -0400

    fix: cambio de tipeos y variables por buenas practicas

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,38 @@
 #include <DHT.h>
 
 //no recomendable usar define ya que lo que hace es reemplazar el texto en el preprocesador, es mejor usar const
-#define DHTPIN 15
-#define DHTTYPE DHT22
+constexpr int DHTPIN = 15;
+constexpr char DHTTYPE = DHT22;
 DHT dht(DHTPIN,DHTTYPE);
 
 //Si el valor se conoce en tiempo de compilación, suele preferirse constexpr para declarar constantes, ya que permite al compilador optimizar el código y mejorar la seguridad del tipo de datos.
-const int pinLDR = 34;
-const int ledRojo = 27;
-const int ledAmarillo = 14;
-const int ledVerde = 12;
+constexpr int PIN_LDR = 34;
+constexpr int LED_ROJO = 27;
+constexpr int LED_AMARILLO = 14;
+constexpr int LED_VERDE = 12;
+bool oscuridadDetectada = false;
 
 void setup()
 {
   Serial.begin(115200);
   dht.begin();
-  pinMode(ledRojo, OUTPUT);
-  pinMode(ledAmarillo, OUTPUT);
-  pinMode(ledVerde, OUTPUT);
+  pinMode(LED_ROJO, OUTPUT);
+  pinMode(LED_AMARILLO, OUTPUT);
+  pinMode(LED_VERDE, OUTPUT);
 }
 
 void loop()
 {
-  int valorLDR = analogRead(pinLDR);
+  int valorLDR = analogRead(PIN_LDR);
   Serial.print("Valor LDR: ");
   Serial.println(valorLDR);
   if(valorLDR > 2500){
-    digitalWrite(ledRojo, HIGH);
+    digitalWrite(LED_ROJO, HIGH);
+    oscuridadDetectada = true;
     Serial.println("Estado: Oscuridad detectada. LED ENCENDIDO");
   }else{
-    digitalWrite(ledRojo, LOW);
+    digitalWrite(LED_ROJO, LOW);
+    oscuridadDetectada = false;
     Serial.println("Estado: Hay luz ambiental. LED APAGADO");
   }
 
@@ -43,23 +46,23 @@ void loop()
   Serial.print("Temperatura actual: ");
   Serial.println(temperatura);
   if(temperatura < 28.0){
-    digitalWrite(ledVerde, HIGH);
-    digitalWrite(ledAmarillo, LOW);
-    if(valorLDR < 2500){
-      digitalWrite(ledRojo, LOW);
+    digitalWrite(LED_VERDE, HIGH);
+    digitalWrite(LED_AMARILLO, LOW);
+    if(!oscuridadDetectada){
+      digitalWrite(LED_ROJO, LOW);
     }
     Serial.println("Estado: clima agradable");
   }else if(temperatura >= 28.0 && temperatura <= 34.0){
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(ledAmarillo, HIGH);
-    if(valorLDR < 2500){
-      digitalWrite(ledRojo, LOW);
+    digitalWrite(LED_VERDE, LOW);
+    digitalWrite(LED_AMARILLO, HIGH);
+    if(!oscuridadDetectada){
+      digitalWrite(LED_ROJO, LOW);
     }
     Serial.println("Estado: clima moderado");
   }else{
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(ledAmarillo, LOW);
-    digitalWrite(ledRojo, HIGH);
+    digitalWrite(LED_VERDE, LOW);
+    digitalWrite(LED_AMARILLO, LOW);
+    digitalWrite(LED_ROJO, HIGH);
     Serial.println("Estado: clima caluroso");
   }
   delay(2000);
